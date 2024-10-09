22e42b24ec5cb37936b481b54b9215ddb94b1cb0
peatjohnston <peatjohnston@gmail.com>
2024-10-09 16:06:55 +0200

    20241009 updating repo

--- ft_printfv1.c
+++ ft_printfv1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf                                          :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: peatjohnston <peatjohnston@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:54:48 by peatjohnsto       #+#    #+#             */
-/*   Updated: 2024/10/08 10:54:49 by peatjohnsto      ###   ########.fr       */
+/*   Updated: 2024/10/08 15:04:35 by peatjohnsto      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,31 @@ int handle_format(char *format, va_list args)
 {
 	char c;
 	c = va_arg(args, int);
+	int in;
+	in = va_arg(args, int);
 }
 
-int ft_printf(const char format, ...)
+int ft_printf(const char *format, ...)
 {
 	va_list args;
 	int counter;
 
 	counter = 0;
 	va_start(args, format);
-	// int ret = vprintf(format, args);
 	while(*format)
 	{
 		if(*format == '%')
 			format++;
-			counter +=handle_format(format, args, counter);
+			counter += handle_format(format, args);
 	}
 	va_end(args);
 	return ret;
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int result = printf("How many %s\n", "characters?");
+// 	printf("%d\n\n", result);
+// }
